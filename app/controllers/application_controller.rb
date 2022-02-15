6680c29ddb554f1e1e350be3a09986788bf4728d
EC2 Default User <ec2-user@ip-172-31-46-241.ec2.internal>
2022-02-15 09:57:13 +0000

    [Update]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
     root_path
   end
 
-  def after_sign_out_path_for(resource)
+  def after_sign_in_path_for(resource)
     book_path
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   devise_for :users
   root to: "homes#top"
-  get "homes/about"
+  get "home/about" => "homes#about"
 
   resources :books, only: [:new, :create, :index, :show, :destroy]
   resources :users, only: [:show, :edit, :update]

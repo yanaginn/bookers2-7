f0e00d1551f9aad0338505e472fd4715946f67ee
EC2 Default User <ec2-user@ip-172-31-46-241.ec2.internal>
2022-03-05 02:39:38 +0000

    [7-aFinish]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,13 +8,13 @@ class BooksController < ApplicationController
   end
 
   def index
+    @book = Book.new
     to  = Time.current.at_beginning_of_day
     from  = (to - 6.day).at_end_of_day
-    @books = Book.all.sort {|a,b| 
-      b.favorites.where(created_at: from...to).size <=> 
+    @books = Book.all.sort {|a,b|
+      b.favorites.where(created_at: from...to).size <=>
       a.favorites.where(created_at: from...to).size
     }
-    @book = Book.new
   end
 
   def create
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,18 +1,18 @@
 class Book < ApplicationRecord
-
-　belongs_to :user
+  
+  belongs_to :user
   has_many :book_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
-  
+
   has_many :week_favorites, -> { where(created_at: ((Time.current.at_end_of_day - 6.day).at_beginning_of_day)..(Time.current.at_end_of_day)) }, class_name: 'Favorite'
-  
+
   validates :title,presence:true
   validates :body,presence:true,length:{maximum:200}
-  
+
   def favorited_by?(user)
     favorites.where(user_id: user.id).exists?
   end
-	
+
   def self.search_for(content, method)
     if method == 'perfect'
       Book.where(title: content)
